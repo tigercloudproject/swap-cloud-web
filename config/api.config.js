5f397645944be1870eb9e5bbd75d177de0432555
sunbeyond <sunbeyond@gmail.com>
2019-10-17 13:45:35 +0800

    fix api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,12 +1,12 @@
 export default {
   // cookie domain,change this to save cookie
-  domain: 'bbx.com',
+  domain: 'test.com',
   mainDomain: '',
-  swapsDomain: 'http://devswap.bbx.com/',
+  swapsDomain: 'http://swap.test.com/',
   // api URL,change this to get Data
   baseUrl: '',
   swapsUrl: 'https://devapi.bbx.com/v1/',
   webSocketUrl: 'wss://devapi.bbx.com/v1/ifcontract/realTime',
   // 如果是合约云则为true
-  isYun: true
+  isYun: false
 }
